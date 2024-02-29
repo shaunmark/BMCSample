fe27945df6536209f15c928504a5e3d18801c08f
ShaunMark <shaunantony1000@gmail.com>
2024-02-29 13:49:30 +0530

    - Added .env to gitignore  - Minor updates to error state and loader component  - Updated Readme file to include .env file instructions for getting started section

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import {StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
 
 export function Loader() {
-  return <Text style={cn.loadingText}>Loading...</Text>;
+  return (
+    <View>
+      <ActivityIndicator size="large" color={'#247BA0'} />
+    </View>
+  );
 }
-
-const cn = StyleSheet.create({
-  loadingText: {
-    color: 'black',
-    fontSize: 16,
-  },
-});
